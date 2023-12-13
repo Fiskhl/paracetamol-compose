271fc3408ff53f89737bce0d6ff455ad9016d245
Diangelion <bryanrichie92@gmail.com>
2023-12-13 11:25:39 +0700

    test 2

---
+++ app/src/main/java/com/example/paracetamol/nav_screen/ArchiveScreen.kt
@@ -0,0 +1,9 @@
+package com.example.paracetamol.nav_screen
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun ArchiveScreen(navController: NavController) {
+
+}
