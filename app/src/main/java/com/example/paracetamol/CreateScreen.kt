3d498c63747e0b282307d1a98eeaa6ec62a006eb
Joshua Hotama <joshuahh554@gmail.com>
2023-12-16 13:57:31 +0700

    new

--- app/src/main/java/com/example/paracetamol/CreateScreen.kt
+++ app/src/main/java/com/example/paracetamol/CreateScreen.kt
@@ -4,15 +4,19 @@ import android.annotation.SuppressLint
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.outlined.Close
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -32,6 +36,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -68,136 +73,167 @@ fun CreateScreen(navController: NavController) {
     Column(
         modifier = Modifier
             .fillMaxSize()
+            .width(100.dp)
             .background(Color(0xFFFFFFFF))
-            .padding(horizontal =  48.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+            .padding(horizontal = 16.dp)
+            .padding(top = 23.dp)
     ) {
-        Text("Create Room",
-            fontSize = 32.sp,
-            fontFamily = poppinsFamily,
-            fontWeight = FontWeight.Bold,
-            textAlign = TextAlign.Center,
-            color = Color.Black,
-        )
-        Text("You can create your room and share it to your group",
-            fontSize = 20.sp,
-            fontFamily = poppinsFamily,
-            fontWeight = FontWeight.Medium,
-            textAlign = TextAlign.Center,
-            color = Color.Black,
-            modifier = Modifier.padding(bottom = 30.dp)
-        )
-        TextField(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(bottom = 16.dp),
-            value = textState,
-            onValueChange = {
-                textState = it
-            },
-            placeholder = {
-                Text(
-                    text = "Organization Name",
-                    fontSize = 16.sp,
-                    fontFamily = poppinsFamily,
-                    fontWeight = FontWeight.Normal,
-                    textAlign = TextAlign.Center,
-                    color = Color(0xFF4B4B4B),
-                )
-            },
-            colors = TextFieldDefaults.textFieldColors(
-                containerColor = Color(0xFFF1F8FF),
-                cursorColor = Color.Black,
-                disabledLabelColor = Color(0xFFF1F8FF),
-                focusedIndicatorColor = Color.Transparent,
-                unfocusedIndicatorColor = Color.Transparent
-            ),
-            shape = RoundedCornerShape(8.dp),
-            singleLine = true,
-        )
-
-        TextField(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(bottom = 16.dp),
-            value = textState,
-            onValueChange = {
-                textState = it
-            },
-            placeholder = {
-                Text(
-                    text = "Description",
-                    fontSize = 16.sp,
-                    fontFamily = poppinsFamily,
-                    fontWeight = FontWeight.Normal,
-                    textAlign = TextAlign.Center,
-                    color = Color(0xFF4B4B4B),
-                )
-            },
-            colors = TextFieldDefaults.textFieldColors(
-                containerColor = Color(0xFFF1F8FF),
-                cursorColor = Color.Black,
-                disabledLabelColor = Color(0xFFF1F8FF),
-                focusedIndicatorColor = Color.Transparent,
-                unfocusedIndicatorColor = Color.Transparent
-            ),
-            shape = RoundedCornerShape(8.dp),
-            singleLine = true,
-        )
-
-
-        TextField(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(bottom = 16.dp),
-            value = textState,
-            onValueChange = {
-                textState = it
-            },
-            placeholder = {
-                Text(
-                    text = "Referral Code",
-                    fontSize = 16.sp,
-                    fontFamily = poppinsFamily,
-                    fontWeight = FontWeight.Normal,
-                    textAlign = TextAlign.Center,
-                    color = Color(0xFF4B4B4B),
-                )
-            },
-            colors = TextFieldDefaults.textFieldColors(
-                containerColor = Color(0xFFF1F8FF),
-                cursorColor = Color.Black,
-                disabledLabelColor = Color(0xFFF1F8FF),
-                focusedIndicatorColor = Color.Transparent,
-                unfocusedIndicatorColor = Color.Transparent
-            ),
-            shape = RoundedCornerShape(8.dp),
-            singleLine = true,
-        )
-
-
-        Button(
-            onClick = {
-                navController.navigate(Screen.SearchScreen.route)
-            },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(top = 16.dp, bottom = 16.dp)
-                .height(55.dp),
-//            enabled = isButtonEnabled,
-            border = BorderStroke(2.dp, Color(0xFF47A7FF)),
-            colors = ButtonDefaults.elevatedButtonColors(
-                contentColor = Color.White
-            ),
+        Row(
+            horizontalArrangement = Arrangement.Start,
         ) {
-            Text("Create Room",
-                color = Color.DarkGray,
-                fontSize = 16.sp,
-                fontFamily = poppinsFamily,)
+            val backButtonIcon: Painter =
+                painterResource(id = R.drawable.ic_back) // back
+
+            Image(
+                painter = backButtonIcon,
+                contentDescription = "Back",
+                modifier = Modifier
+                    .size(34.dp)
+                    .padding(start = 17.dp)
+                    .clickable { navController.navigateUp() }
+            )
+
         }
 
 
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color(0xFFFFFFFF))
+                .padding(horizontal = 16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                "Create Room",
+                fontSize = 32.sp,
+                fontFamily = poppinsFamily,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center,
+                color = Color.Black,
+            )
+            Text(
+                "You can create your room and share it to your group",
+                fontSize = 20.sp,
+                fontFamily = poppinsFamily,
+                fontWeight = FontWeight.Medium,
+                textAlign = TextAlign.Center,
+                color = Color.Black,
+                modifier = Modifier.padding(bottom = 30.dp)
+            )
+            TextField(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 16.dp),
+                value = textState,
+                onValueChange = {
+                    textState = it
+                },
+                placeholder = {
+                    Text(
+                        text = "Organization Name",
+                        fontSize = 16.sp,
+                        fontFamily = poppinsFamily,
+                        fontWeight = FontWeight.Normal,
+                        textAlign = TextAlign.Center,
+                        color = Color(0xFF4B4B4B),
+                    )
+                },
+                colors = TextFieldDefaults.textFieldColors(
+                    containerColor = Color(0xFFF1F8FF),
+                    cursorColor = Color.Black,
+                    disabledLabelColor = Color(0xFFF1F8FF),
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent
+                ),
+                shape = RoundedCornerShape(8.dp),
+                singleLine = true,
+            )
+
+            TextField(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 16.dp),
+                value = textState,
+                onValueChange = {
+                    textState = it
+                },
+                placeholder = {
+                    Text(
+                        text = "Description",
+                        fontSize = 16.sp,
+                        fontFamily = poppinsFamily,
+                        fontWeight = FontWeight.Normal,
+                        textAlign = TextAlign.Center,
+                        color = Color(0xFF4B4B4B),
+                    )
+                },
+                colors = TextFieldDefaults.textFieldColors(
+                    containerColor = Color(0xFFF1F8FF),
+                    cursorColor = Color.Black,
+                    disabledLabelColor = Color(0xFFF1F8FF),
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent
+                ),
+                shape = RoundedCornerShape(8.dp),
+                singleLine = true,
+            )
+
+
+            TextField(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 16.dp),
+                value = textState,
+                onValueChange = {
+                    textState = it
+                },
+                placeholder = {
+                    Text(
+                        text = "Referral Code",
+                        fontSize = 16.sp,
+                        fontFamily = poppinsFamily,
+                        fontWeight = FontWeight.Normal,
+                        textAlign = TextAlign.Center,
+                        color = Color(0xFF4B4B4B),
+                    )
+                },
+                colors = TextFieldDefaults.textFieldColors(
+                    containerColor = Color(0xFFF1F8FF),
+                    cursorColor = Color.Black,
+                    disabledLabelColor = Color(0xFFF1F8FF),
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent
+                ),
+                shape = RoundedCornerShape(8.dp),
+                singleLine = true,
+            )
+
+
+            Button(
+                onClick = {
+                    navController.navigate(Screen.SearchScreen.route)
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 16.dp, bottom = 16.dp)
+                    .height(55.dp),
+//            enabled = isButtonEnabled,
+                border = BorderStroke(2.dp, Color(0xFF47A7FF)),
+                colors = ButtonDefaults.elevatedButtonColors(
+                    contentColor = Color.White
+                ),
+            ) {
+                Text(
+                    "Create Room",
+                    color = Color.DarkGray,
+                    fontSize = 16.sp,
+                    fontFamily = poppinsFamily,
+                )
+            }
+
+
+        }
     }
 }
 
--- app/src/main/java/com/example/paracetamol/JoinScreen.kt
+++ app/src/main/java/com/example/paracetamol/JoinScreen.kt
@@ -4,12 +4,15 @@ import android.annotation.SuppressLint
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
@@ -32,6 +35,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -61,78 +65,108 @@ fun JoinScreen(navController: NavController) {
     Column(
         modifier = Modifier
             .fillMaxSize()
+            .width(100.dp)
             .background(Color(0xFFFFFFFF))
-            .padding(horizontal =  48.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+            .padding(horizontal = 16.dp)
+            .padding(top = 23.dp)
     ) {
-        Text("Join Room",
-            fontSize = 32.sp,
-            fontFamily = poppinsFamily,
-            fontWeight = FontWeight.Bold,
-            textAlign = TextAlign.Center,
-            color = Color.Black,
-        )
-        Text("Give me your code and i will give your group",
-            fontSize = 20.sp,
-            fontFamily = poppinsFamily,
-            fontWeight = FontWeight.Medium,
-            textAlign = TextAlign.Center,
-            color = Color.Black,
-            modifier = Modifier.padding(bottom = 30.dp)
-        )
-
-
-        TextField(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(bottom = 16.dp),
-            value = textState,
-            onValueChange = {
-                textState = it
-            },
-            placeholder = {
-                Text(
-                    text = "Referral Code",
-                    fontSize = 16.sp,
-                    fontFamily = poppinsFamily,
-                    fontWeight = FontWeight.Normal,
-                    textAlign = TextAlign.Center,
-                    color = Color(0xFF4B4B4B),
-                )
-            },
-            colors = TextFieldDefaults.textFieldColors(
-                containerColor = Color(0xFFFFF1F1),
-                cursorColor = Color.Black,
-                disabledLabelColor = Color(0xFFFFF1F1),
-                focusedIndicatorColor = Color.Transparent,
-                unfocusedIndicatorColor = Color.Transparent
-            ),
-            shape = RoundedCornerShape(8.dp),
-            singleLine = true,
-        )
-
-
-        Button(
-            onClick = {
-                navController.navigate(Screen.SearchScreen.route)
-            },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(top = 16.dp, bottom = 16.dp)
-                .height(55.dp),
-            border = BorderStroke(2.dp, Color.Red),
-            colors = ButtonDefaults.elevatedButtonColors(
-                contentColor = Color.White
-            ),
+        Row(
+            horizontalArrangement = Arrangement.Start,
         ) {
-            Text("Join Room",
-                color = Color.DarkGray,
-                fontSize = 16.sp,
-                fontFamily = poppinsFamily,)
+            val backButtonIcon: Painter =
+                painterResource(id = R.drawable.ic_back) // back
+
+            Image(
+                painter = backButtonIcon,
+                contentDescription = "Back",
+                modifier = Modifier
+                    .size(34.dp)
+                    .padding(start = 17.dp)
+                    .clickable { navController.navigateUp() }
+            )
         }
 
 
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color(0xFFFFFFFF))
+                .padding(horizontal = 48.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                "Join Room",
+                fontSize = 32.sp,
+                fontFamily = poppinsFamily,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center,
+                color = Color.Black,
+            )
+            Text(
+                "Give me your code and i will give your group",
+                fontSize = 20.sp,
+                fontFamily = poppinsFamily,
+                fontWeight = FontWeight.Medium,
+                textAlign = TextAlign.Center,
+                color = Color.Black,
+                modifier = Modifier.padding(bottom = 30.dp)
+            )
+
+
+            TextField(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 16.dp),
+                value = textState,
+                onValueChange = {
+                    textState = it
+                },
+                placeholder = {
+                    Text(
+                        text = "Referral Code",
+                        fontSize = 16.sp,
+                        fontFamily = poppinsFamily,
+                        fontWeight = FontWeight.Normal,
+                        textAlign = TextAlign.Center,
+                        color = Color(0xFF4B4B4B),
+                    )
+                },
+                colors = TextFieldDefaults.textFieldColors(
+                    containerColor = Color(0xFFFFF1F1),
+                    cursorColor = Color.Black,
+                    disabledLabelColor = Color(0xFFFFF1F1),
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent
+                ),
+                shape = RoundedCornerShape(8.dp),
+                singleLine = true,
+            )
+
+
+            Button(
+                onClick = {
+                    navController.navigate(Screen.SearchScreen.route)
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 16.dp, bottom = 16.dp)
+                    .height(55.dp),
+                border = BorderStroke(2.dp, Color.Red),
+                colors = ButtonDefaults.elevatedButtonColors(
+                    contentColor = Color.White
+                ),
+            ) {
+                Text(
+                    "Join Room",
+                    color = Color.DarkGray,
+                    fontSize = 16.sp,
+                    fontFamily = poppinsFamily,
+                )
+            }
+
+
+        }
     }
 }
 
