905a47b81a988dd7bfa7c97a1ae19d639d01b606
Kafijaya <kafijaya@student.umn.ac.id>
2023-12-21 02:55:29 +0700

    Komentar pada Folder component

--- app/src/main/java/com/example/paracetamol/component/Toast.kt
+++ app/src/main/java/com/example/paracetamol/component/Toast.kt
@@ -4,6 +4,12 @@ package com.example.paracetamol.component
 import android.content.Context
 import android.widget.Toast
 
+/**
+ * Displays a short-duration toast message in the given [context] with the specified [message].
+ *
+ * @param context The context in which the toast should be displayed.
+ * @param message The message to be shown in the toast.
+ */
 fun showToast(context: Context, message: String) {
     Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
 }
