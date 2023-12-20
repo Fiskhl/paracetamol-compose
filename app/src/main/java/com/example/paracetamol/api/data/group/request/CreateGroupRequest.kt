70452df00dd3e0ca4a216bf5e4ca05a34604fef1
Kafijaya <kafijaya@student.umn.ac.id>
2023-12-21 02:48:54 +0700

    Komentar pada Folder API

--- app/src/main/java/com/example/paracetamol/api/data/group/request/CreateGroupRequest.kt
+++ app/src/main/java/com/example/paracetamol/api/data/group/request/CreateGroupRequest.kt
@@ -1,5 +1,12 @@
 package com.example.paracetamol.api.data.group.request
 
+/**
+ * Data class representing the request for creating a group.
+ * @property namaGroup The name of the group.
+ * @property refKey The reference key of the group.
+ * @property desc The description of the group.
+ * @property status The status of the group.
+ */
 data class CreateGroupRequest(
     val namaGroup: String,
     val refKey: String,
--- app/src/main/java/com/example/paracetamol/api/data/register/RegisterRequest.kt
+++ app/src/main/java/com/example/paracetamol/api/data/register/RegisterRequest.kt
@@ -1,5 +1,14 @@
 package com.example.paracetamol.api.data.register
 
+/**
+ * Data class representing the request for registering.
+ * @property nama The name of the user.
+ * @property nim The NIM of the user.
+ * @property password The password of the user.
+ * @property email The email of the user.
+ * @property prodi The study program of the user.
+ * @property angkatan The year of the user.
+ */
 data class RegisterRequest(
     val nama: String,
     val nim: String,
